ce1d0d577b798812e63a8ce2c199e4193ada9b07
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-25 08:08:14 +0530

    initial commit for spring state machine starter

---
+++ sb_state_machine_starter/src/main/java/com/ssm/SbStateMachineStarterApplication.java
@@ -0,0 +1,14 @@
+package com.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStateMachineStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbStateMachineStarterApplication.class, args);
+		System.out.println("SPRING\n STATE\n MACHINE");
+	}
+
+}
---
+++ sb_state_machine_starter/src/main/java/com/ssm/domain/PaymentState.java
@@ -0,0 +1,8 @@
+package com.ssm.domain;
+/**
+ * @author Naveen K Wodeyar
+ * @date 25-Jul-2024
+ */
+public class PaymentState {
+
+}
